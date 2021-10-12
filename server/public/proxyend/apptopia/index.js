abbc5f56d6885ed63416395a48cbe50bfee87a11
huanhuanboy <weihuan.luositing@gmail.com>
2021-10-12 23:01:07 +0800

    .

---
+++ server/public/proxyend/apptopia/index.js
@@ -0,0 +1,50 @@
+const websites = [
+    {
+        "name":"Tesla",
+        "url":"https://apptopia.com/apps/unified/8080952667/performance"
+    },
+    {
+        "name":"FordPass",
+        "url":"https://apptopia.com/apps/unified/8080359488/performance"
+    },
+    {
+        "name":"BMW Connected",
+        "url":"https://apptopia.com/apps/unified/8080420189/performance"
+    },
+    {
+        "name":"My Volkswagen",
+        "url":"https://apptopia.com/apps/unified/8138999265/performance"
+    },
+    {
+        "name":"Volkswagen We Connect",
+        "url":"https://apptopia.com/apps/unified/8080497220/performance"
+    },
+    {
+        "name":"Volkswagen Connect",
+        "url":"https://apptopia.com/apps/unified/8080497220/performance"
+    },
+    {
+        "name":"uconnect",
+        "url":"https://apptopia.com/apps/unified/8080684563/performance"
+    },
+    {
+        "name":"Fiat GOe live",
+        "url":"https://apptopia.com/apps/unified/8180881117/performance"
+    },
+    {
+        "name":"myBuick",
+        "url":"https://apptopia.com/apps/unified/8080776812/performance"
+    },
+    {
+        "name":"myCadillac",
+        "url":"https://apptopia.com/apps/unified/8080259231/performance"
+    },
+    {
+        "name":"myChevrolet",
+        "url":"https://apptopia.com/apps/unified/8080736369/performance"
+    },
+    {
+        "name":"myGMC",
+        "url":"https://apptopia.com/apps/unified/8080993288/performance"
+    }
+]
